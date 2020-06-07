253c7b5ff5c6d13e4f7f358e73901a70da275e3c
Don <kartik15nov@gmail.com>
2020-06-07 20:01:32 +0530

    Converting to WebFlux

--- src/main/java/com/unknownbrain/recipeapp/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/unknownbrain/recipeapp/controllers/ControllerExceptionHandler.java
@@ -3,10 +3,11 @@ package com.unknownbrain.recipeapp.controllers;
 import com.unknownbrain.recipeapp.exceptions.NotFoundException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.servlet.ModelAndView;
 
 @Log4j2
 @ControllerAdvice
@@ -14,31 +15,36 @@ public class ControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormatException(Exception exception) {
+    public String handleNumberFormatException(Model model, Exception exception) {
 
         log.error("Handling Number Format exception");
         log.error("exception", exception);
 
-        ModelAndView modelAndView = new ModelAndView();
+//        ModelAndView modelAndView = new ModelAndView();
 
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
+//        modelAndView.setViewName("400error");
+//        modelAndView.addObject("exception", exception);
+//
+//        return modelAndView;
+        model.addAttribute("exception", exception);
+        return "400error";
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception) {
+    public String handleNotFound(Model model, Exception exception) {
 
         log.error("Handling not found exception");
         log.error("exception", exception);
 
-        ModelAndView modelAndView = new ModelAndView();
+//        ModelAndView modelAndView = new ModelAndView();
 
-        modelAndView.setViewName("404error");
-        modelAndView.addObject("exception", exception);
+//        modelAndView.setViewName("404error");
+//        modelAndView.addObject("exception", exception);
 
-        return modelAndView;
+//        return modelAndView;
+
+        model.addAttribute("exception", exception);
+        return "404error";
     }
 }
